87cb4bfe8374ea4295e066689c182ee589ddfdab
BobJ911 <bobbyjiangt@gmail.com>
2022-08-12 23:50:33 -0700

    School Project and Unit 3 Assignment updated

---
+++ SchoolProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        School addstudents = new School();
+        School addteachers = new School(0);
+        School showstudentsandteacher = new School(0,0,0,0);
+        School removestudents = new School(0,0,0,0,0);
+        School removeteachers = new School(0,0,0,0,0,0);
+        School showstudentsandteachers = new School(0,0,0,0);
+    }
+}
--- Unit3Assignment/src/Main.java
+++ Unit3Assignment/src/Main.java
@@ -1,25 +1,49 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Scanner s1 = new Scanner(System.in);
+        System.out.println("Please enter the size of the array");
+        int arr1length = s1.nextInt();
+        int[] arr1 = new int [arr1length];
+        System.out.println("Please enter numbers into the array");
+        for (int i = 0; i < arr1.length; i++){
+            arr1[i] = s1.nextInt();
+        }
+        System.out.println("The array is now full");
+
+
+        System.out.println("Please enter how many numbers you would like to add/subtract");
         int user = s1.nextInt();
+        System.out.println("Please enter the position of the number you would like to insert");
         int position = s1.nextInt();
+        System.out.println("Please enter the number your would like to insert");
         int number = s1.nextInt();
-        int[] arr2 = new int[arr1.length + user];
 
         if (user > 0) {
-           add(arr1, arr2, position);
-           insertnumber(arr2,position, number);
-           print(arr2);
+            add(arr1, user);
+            insert(arr1, add(arr1, user), position, number);
+            for(int i = 0; i <add(arr1, user).length; i ++){
+                System.out.println(insert(arr1, delete(arr1, user), position, number)[i]);
+            }
         }else {
-            delete(arr1, arr2, user, position);
-            insertnumber(arr2,position, number);
-            print(arr2);
+            delete(arr1, user);
+            insert(arr1, delete(arr1, user), position, number);
+            for(int i = 0; i <delete(arr1, user).length; i ++){
+                System.out.println(insert(arr1, delete(arr1, user), position, number)[i]);
+            }
         }
     }
+    private static int[] add(int[] arr1, int user) {
+        int[] arr2 = new int[arr1.length + user];
+        return arr2;
+    }
 
-    private static void add(int[] arr1, int[] arr2, int position) {
+    private static int[] delete(int[] arr1,int user){
+        int[] arr2 = new int[arr1.length + user];
+        return arr2;
+    }
+
+    private static int[] insert(int[] arr1, int[] arr2, int position, int number){
         for (int i = 0; i < arr1.length; i++) {
             if (i >= position) {
                 if (i == arr2.length - 1) {
@@ -31,28 +55,7 @@ public class Main {
                 arr2[i] = arr1[i];
             }
         }
-    }
-    private static void delete(int[] arr1,int[] arr2, int user, int position){
-        for (int i = 0; i < arr1.length+user; i++) {
-            if (i >= position) {
-                if(i == arr2.length-1) {
-                    break;
-                }else{
-                    arr2[i + 1] = arr1[i];
-                }
-            } else {
-                arr2[i] = arr1[i];
-            }
-        }
-    }
-
-    private static void insertnumber(int[] arr2, int position, int number){
         arr2[position] = number;
-    }
-
-    private static void print(int[] arr2){
-        for(int i = 0; i<arr2.length; i++){
-            System.out.print(arr2[i] + "\t");
-        }
+        return arr2;
     }
 }
